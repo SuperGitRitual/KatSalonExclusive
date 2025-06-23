ff4a5026630ac04b1aa923a616f98974ea89bae5
SuperGitRitual <m.macias.mx@proton.me>
2025-06-22 18:42:54 -0600

    Primer commit de mi sitio web limpio y completo

---
+++ script.js
@@ -0,0 +1,8 @@
+<<<<<<< HEAD
+document.getElementById('whatsappBtn').addEventListener('click', function() {
+    window.open('https://wa.me/5215555555555', '_blank');
+=======
+document.getElementById('whatsappBtn').addEventListener('click', function() {
+    window.open('https://wa.me/5215555555555', '_blank');
+>>>>>>> 3133ad1f49143c908c88fff4b7349a0bf728cbed
+  });
